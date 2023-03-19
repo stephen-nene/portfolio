189487a98f75b45f3b95d78f2480bdf5c6885328
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-19 12:22:08 +0300

    new resume

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+// import React, { useContext } from 'react';
 // import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
