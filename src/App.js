5567da7768e16c09369c8d6fe72007a3c49df91a
stephen-nene <stephen.nene@student.moringaschool.com>
2023-04-15 07:54:40 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import React, { useContext } from 'react';
 // import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-
+import Navbar from "./components/Navbar"
+import Intro from "./components/Intro";
+import BackToTop from "./components/back-top";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>hello people</h1>
+      <Intro/>
+
+      <BackToTop/>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar  (){
+
+    return (
+        <div>
+            <h2>navbar</h2>
+        </div>
+    )
+}
