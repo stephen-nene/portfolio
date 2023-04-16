1dbed8cace8d6475ad1f3063cdeff9b294840e3c
stephen-nene <stephen.nene@student.moringaschool.com>
2023-04-16 13:45:11 +0300

    added a nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default function Navbar  (){
+export default function Navigation  (){
 
     return (
         <div>
-            <h2>navbar</h2>
+            {/* <h2>navbar</h2> */}
+            <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            {/* <img
+              alt=""
+              src="../assets/svg/about/blueTheme-boy.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{'  '} */}
+            Stephen Nene
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
         </div>
     )
 }
